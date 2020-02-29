79911c5ce1fee54b6aecc57bae2733fbaf4822cb
Terence Wang <terence79119@gmail.com>
2020-02-29 16:02:27 +0800

    '修正國家沒有用成字串的部份'

--- driving.py
+++ driving.py
@@ -3,12 +3,12 @@
 age = input('請輸入年齡: ')
 age = int(age)
 nat = input('請輸入您的國家: ')
-if nat == Taiwan:
+if nat == 'Taiwan':
 	if age >= 18:
 		print('你可以開車')
 	else:
 		print('你還不可以開車')
-elif nat == America:
+elif nat == 'America':
 	if age >= 16:
 		print('你可以開車')
 	else:
